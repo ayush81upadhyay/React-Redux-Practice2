6e79c6f23640714f0e31ea6ea9a6cdb4f7cf5720
DEVD <ayush7upadhyay@gmail.com>
2024-11-11 09:14:14 +0530

    Added CountryDetail Page and used React Router DOM

---
+++ 15_Rest_Countries_React_Router_DOM_Further/Components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = ()=> {
+  return (
+    <div style={{marginLeft: 425, marginTop: 200}}>
+        <h1>Developed by Ayush Upadhyay</h1>
+        <h2>Contact at github@ayush81upadhyay</h2>
+    </div>
+  )
+}
+
+export default Contact
